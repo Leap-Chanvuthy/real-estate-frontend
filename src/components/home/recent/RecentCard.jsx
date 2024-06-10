b62140c26e1c9b03e51095094393990bcc5fe305
panhareachphat <panhareach.phat@bi-kay.com>
2024-06-10 14:19:16 +0700

    fix: typo

--- src/components/home/recent/RecentCard.jsx
+++ src/components/home/recent/RecentCard.jsx
@@ -108,7 +108,7 @@ const RecentCard = () => {
   const fetchProperties = async (page = currentPage) => {
     dispatch(fetchPropertiesStart());
     try {
-      const response = await axios.get(`http://127.0.0.1:8001/api/properties/?page=${page}`);
+      const response = await axios.get(`http://127.0.0.1:8000/api/properties/?page=${page}`);
       console.log("API Response:", response.data);
       dispatch(fetchPropertiesSuccess(response.data));
       setCurrentPage(response.data.meta.current_page);
