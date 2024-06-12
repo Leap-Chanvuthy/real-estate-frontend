c86b2f181b83f7ec175e0a2e896ae2634f718769
Elon Thy <115206732+Leap-Chanvuthy@users.noreply.github.com>
2024-06-12 18:11:35 +0700

    enable 2fa

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 import Alert from "@mui/material/Alert";
-import {CircularProgress , Button} from '@mui/material';
+import { CircularProgress, Button } from "@mui/material";
 import { persistor } from "../../redux/store";
 import { logout } from "../../redux/slice/authSlice";
 import TextField from "@mui/material/TextField";
@@ -9,6 +9,7 @@ import { updateUserProfile } from "../../redux/slice/authSlice";
 import axios from "axios";
 import UpdateProfile from "./components/UpdateProfile";
 import UpdatePassword from "./components/UpdatePassword";
+import Enable2FA from "./components/Enable2FA";
 
 const Profile = () => {
   const { currentUser } = useSelector((state) => state.auth);
@@ -30,8 +31,8 @@ const Profile = () => {
   });
 
   const [errorArray, setErrorArray] = useState([]);
-  const [success , setSuccess] = useState(false);
-  const [loading , setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   console.log(errorArray);
   const handleChange = (e) => {
@@ -63,7 +64,7 @@ const Profile = () => {
         setLoading(false);
         setSuccess(true);
         setTimeout(() => {
-            setSuccess(false)
+          setSuccess(false);
         }, 3000);
       }
     } catch (error) {
@@ -75,43 +76,11 @@ const Profile = () => {
 
   return (
     <div className="mx-10">
-      
-      <UpdateProfile/>
+      <UpdateProfile />
       <hr />
-
-      <UpdatePassword/>
-
+      <UpdatePassword />
       <hr />
-
-      <div className="flex flex-col lg:md:flex-row justify-between items-start gap-10 my-10">
-        <div className="w-full">
-          <h3 className="text-xl font-bold">Two Factors Authetentication</h3>
-          <p>
-            Add Extra Layer With Two Factors Authetentication. Use Google
-            Authenticators Secure Your Account.
-          </p>
-          <button className="bg-slate-800 px-4 py-2  mt-5 rounded-sm text-white">
-            Enable
-          </button>
-        </div>
-        <div className="w-full ">
-          <div className="flex">
-            <img
-              className="w-[160px]"
-              src="https://cdn.britannica.com/17/155017-050-9AC96FC8/Example-QR-code.jpg"
-            />
-            {/* <div className="mt-5">
-                <strong>Steps :</strong>
-                <ul>
-                  <li>Click Enable</li>
-                  <li>Scan QR Code In Google Authenticator</li>
-                  <li>Enter Your OTP Code</li>
-                  <li className="font-bold text-green-500">Done ✔</li>
-                </ul>
-              </div> */}
-          </div>
-        </div>
-      </div>
+      <Enable2FA/>
     </div>
   );
 };
---
+++ src/components/profile/components/Enable2FA.jsx
@@ -0,0 +1,72 @@
+import axios from "axios";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import {CircularProgress , Button} from '@mui/material';
+
+const Enable2FA = () => {
+  const { currentUser } = useSelector((state) => state.auth);
+  const authorisation = currentUser?.authorisation;
+
+  const [QRImage, setQRImage] = useState(null);
+  const [loading , setLoading] = useState(false);
+
+  const handleEnable2FA = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/enable2fa",
+        null,
+        {
+          headers: { Authorization: `Bearer ${authorisation.token}` },
+        }
+      );
+
+      if (response.status === 200) {
+        setLoading(false);
+        setQRImage(response.data.QR_Image); // Assuming QRImage is returned from server
+        console.log(response);
+      }
+    } catch (error) {
+      console.error("Error enabling 2FA:", error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col lg:md:flex-row justify-between items-start gap-10 my-10">
+      <div className="w-full">
+        <h3 className="text-xl font-bold">Two Factors Authentication</h3>
+        <p>
+          Add an Extra Layer With Two Factors Authentication. Use Google
+          Authenticator to Secure Your Account.
+        </p>
+        <div className="mt-5">
+            <Button variant="outlined" size="medium" onClick={handleEnable2FA}>
+              {loading ? <CircularProgress size={25} /> : "Enable"}
+            </Button>
+        </div>
+        <div className="mt-5">
+          <strong>Steps :</strong>
+          <ul>
+            <li>Click Enable</li>
+            <li>Scan QR Code In Google Authenticator</li>
+            <li>Enter Your OTP Code</li>
+            <li className="font-bold text-green-500">Done ✔</li>
+          </ul>
+        </div>
+      </div>
+      <div className="w-full ">
+        <div className="flex">
+          {QRImage && (
+            <div
+              dangerouslySetInnerHTML={{ __html: QRImage }}
+              style={{ maxWidth: "200px" }} // Set a max width for the QR code image
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Enable2FA;
