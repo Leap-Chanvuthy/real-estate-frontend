09442f594613a49418609a3d67c1b33b3459db0b
Elon Thy <115206732+Leap-Chanvuthy@users.noreply.github.com>
2024-06-23 17:36:09 +0700

    modify base url

--- src/constants/const.js
+++ src/constants/const.js
@@ -1,6 +1,6 @@
 // export const BASE_URL = 'http://localhost:8000/api';
-export const BASE_URL = 'http://localhost:8001/api'
+export const BASE_URL = 'http://localhost:8000/api'
 // export const BASE_IMAGE_URL = 'http://localhost:8000'
-export const BASE_IMAGE_URL = 'http://localhost:8001'
+export const BASE_IMAGE_URL = 'http://localhost:8000'
 
 export const DEFAULT_IMAGE = 'https://img.freepik.com/premium-vector/real-estate-business-concept-with-houses-real-estate-market-growth-flat-modern-design-illustration_566886-594.jpg'
