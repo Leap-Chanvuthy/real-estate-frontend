64435cf968c386d8d340b29834b6e58a808db263
Elon Thy <115206732+Leap-Chanvuthy@users.noreply.github.com>
2024-08-02 15:58:49 +0700

    modify base url in profile page

--- src/components/profile/components/UpdatePassword.jsx
+++ src/components/profile/components/UpdatePassword.jsx
@@ -1,123 +1,3 @@
-// import TextField from "@mui/material/TextField";
-// import { CircularProgress, Button } from "@mui/material";
-// import Alert from "@mui/material/Alert";
-// import { useState } from "react";
-// import axios from "axios";
-// import { useSelector , useDispatch } from "react-redux";
-
-// const UpdatePassword = () => {
-
-//     const { currentUser } = useSelector((state) => state.auth);
-//     const authorisation = currentUser?.authorisation;
-
-//   const [values, setValues] = useState({
-//     current_password: "",
-//     new_password: "",
-//     password_confirmation: "",
-//   });
-
-//   const [errorArray, setErrorArray] = useState([]);
-//   const [success, setSuccess] = useState(false);
-//   const [loading, setLoading] = useState(false);
-
-//   const handleChange = (e) => {
-//     const { id, value } = e.target;
-//     setValues({ ...values, [id]: value });
-//   };
-
-//   const handleChagePassword = async (e) => {
-//     e.preventDefault();
-//     setLoading(true);
-//     try {
-//       const response = await axios.patch(
-//         "http://127.0.0.1:8000/api/profile/change-password",
-//         values,
-//         {
-//             headers: { Authorization: `Bearer ${authorisation.token}` },
-//           }
-//       );
-//       console.log(response);
-//       if (response.status === 200) {
-//         setLoading(false);
-//         setSuccess(true);
-//         setTimeout(() => {
-//             setSuccess(false);
-//         }, 3000);
-//         setValues({
-//           current_password: "",
-//           new_password: "",
-//           password_confirmation: "",
-//         });
-//       }
-//     } catch (error) {
-//       setErrorArray(error.response.data.errors);
-//       console.log(error);
-//       setLoading(false);
-//     }
-//   };
-
-//   return (
-//     <div className="flex flex-col lg:md:flex-row justify-between items-start gap-10 my-10">
-//       <div className="w-full">
-//         <h3 className="text-xl font-bold">Password</h3>
-//         <p>Password Must Be At Least 8 Characters Long</p>
-//       </div>
-//       <form className="w-full" onSubmit={handleChagePassword}>
-//         {success && (
-//           <Alert severity="success" color="warning" className="w-full my-5">
-//             Password Updated Successfully !
-//           </Alert>
-//         )}
-//         <TextField
-//           id="current_password"
-//           label="Current Password"
-//           size="medium"
-//           error={Boolean(errorArray?.current_password)}
-//           helperText={
-//             errorArray?.current_password
-//               ? errorArray.curr?.current_password.join(", ")
-//               : ""
-//           }
-//           className="w-full"
-//           variant="outlined"
-//           value={values.current_password}
-//           onChange={handleChange}
-//         />
-//         <div className="flex gap-5 mt-5 mb-3">
-//           <TextField
-//             id="new_password"
-//             label="New Password"
-//             size="medium"
-//             error={Boolean(errorArray?.new_password)}
-//             helperText={errorArray?.new_password ? errorArray.new_password.join(", ") : ""}
-//             className="w-full"
-//             variant="outlined"
-//             value={values.new_password}
-//             onChange={handleChange}
-//           />
-//           <TextField
-//             id="password_confirmation"
-//             label="Confirm New Password"
-//             size="medium"
-//             error={Boolean(errorArray?.password_confirmation)}
-//             helperText={errorArray?.password_confirmation ? errorArray.password_confirmation.join(", ") : ""}
-//             className="w-full"
-//             variant="outlined"
-//             value={values.password_confirmation}
-//             onChange={handleChange}
-//           />
-//         </div>
-//         <Button variant="outlined" size="medium" type="submit">
-//           {loading ? <CircularProgress size={25} /> : "Update"}
-//         </Button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default UpdatePassword;
-
-
 import TextField from "@mui/material/TextField";
 import { CircularProgress, Button, Snackbar } from "@mui/material";
 import MuiAlert from "@mui/material/Alert";
@@ -125,6 +5,7 @@ import { useState } from "react";
 import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
 import { updateUserProfile } from "../../../redux/slice/authSlice";
+import { BASE_URL } from "../../../constants/const";
 
 const UpdatePassword = () => {
   const { currentUser } = useSelector((state) => state.auth);
@@ -150,7 +31,7 @@ const UpdatePassword = () => {
     setLoading(true);
     try {
       const response = await axios.patch(
-        "http://127.0.0.1:8000/api/profile/change-password",
+        `${BASE_URL}/profile/change-password`,
         values,
         {
           headers: { Authorization: `Bearer ${authorisation.token}` },
--- src/components/profile/components/UpdateProfile.jsx
+++ src/components/profile/components/UpdateProfile.jsx
@@ -1,135 +1,3 @@
-// import { useSelector, useDispatch } from "react-redux";
-// import Alert from "@mui/material/Alert";
-// import {CircularProgress , Button} from '@mui/material';
-// import TextField from "@mui/material/TextField";
-// import { useState } from "react";
-// import { updateUserProfile } from "../../../redux/slice/authSlice";
-// import axios from "axios";
-
-
-
-
-// const UpdateProfile = () => {
-//     const { currentUser } = useSelector((state) => state.auth);
-//     const dispatch = useDispatch();
-//     const user = currentUser?.user;
-//     const authorisation = currentUser?.authorisation;
-  
-//     // Current User Credential
-//     const [values, setValues] = useState({
-//       name: user.name,
-//       email: user.email,
-//       phone_number: user.phone_number,
-//     });
-  
-
-//     // Update Profile
-//     const [errorArray, setErrorArray] = useState([]);
-//     const [success , setSuccess] = useState(false);
-//     const [loading , setLoading] = useState(false);
-  
-//     console.log(errorArray);
-//     const handleChange = (e) => {
-//       const key = e.target.id;
-//       const value = e.target.value;
-//       setValues({ ...values, [key]: value });
-//     };
-  
-//     const handleUpdateProfile = async (e) => {
-//       e.preventDefault();
-//       try {
-//         setLoading(true);
-//         const response = await axios.patch(
-//           "http://127.0.0.1:8000/api/profile/update",
-//           values,
-//           {
-//             headers: { Authorization: `Bearer ${authorisation.token}` },
-//           }
-//         );
-//         console.log(response);
-//         if (response.status == 200) {
-//           dispatch(
-//             updateUserProfile({
-//               authorisation: authorisation,
-//               user: response.data.data,
-//             })
-//           );
-//           setLoading(false);
-//           setSuccess(true);
-//           setTimeout(() => {
-//               setSuccess(false)
-//           }, 3000);
-//         }
-//       } catch (error) {
-//         console.error("Error updating profile:", error);
-//         setErrorArray(error.response.data.errors);
-//         setLoading(false);
-//       }
-//     };
-
-//     return ( 
-//         <div className="flex flex-col lg:md:flex-row justify-between items-start gap-10 my-10">
-//         <div className="w-full">
-//           <h3 className="text-xl font-bold">Profile Information</h3>
-//           <p>Update Your Profile Infos</p>
-//         </div>
-//         <form className="w-full" onSubmit={handleUpdateProfile}>
-//         {success && (
-//             <Alert severity="success" color="warning" className="w-full my-5">
-//               Profile Updated Successfully !
-//             </Alert>
-//           )}
-//           <TextField
-//             id="name"
-//             label="Username"
-//             size="medium"
-//             className="w-full"
-//             variant="outlined"
-//             error={Boolean(errorArray?.name)}
-//             helperText={errorArray?.name ? errorArray.name.join(", ") : ""}
-//             value={values.name}
-//             onChange={handleChange}
-//           />
-//           <div className="flex gap-5 mt-5 mb-3">
-//             <TextField
-//               id="email"
-//               label="Email"
-//               size="medium"
-//               className="w-full"
-//               variant="outlined"
-//               error={Boolean(errorArray?.email)}
-//               helperText={errorArray?.email ? errorArray.email.join(", ") : ""}
-//               value={values.email}
-//               onChange={handleChange}
-//             />
-//             <TextField
-//               id="phone_number"
-//               label="Phone Number"
-//               size="medium"
-//               className="w-full"
-//               variant="outlined"
-//               error={Boolean(errorArray?.phone_number)}
-//               helperText={errorArray?.phone_number ? errorArray.phone_number.join(", ") : ""}
-//               value={values.phone_number}
-//               onChange={handleChange}
-//             />
-//           </div>
-//           <Button
-//             variant="outlined"
-           
-//             size="medium"
-//             type="submit"
-//           >
-//             {loading ? <CircularProgress size={25}/> : 'Update'}
-//           </Button>
-//         </form>
-//       </div>
-//      );
-// }
- 
-// export default UpdateProfile;
-
-
 import { useSelector, useDispatch } from "react-redux";
 import { Snackbar, CircularProgress, Button } from '@mui/material';
 import MuiAlert from "@mui/material/Alert";
@@ -137,6 +5,7 @@ import TextField from "@mui/material/TextField";
 import { useState } from "react";
 import { updateUserProfile } from "../../../redux/slice/authSlice";
 import axios from "axios";
+import { BASE_URL } from "../../../constants/const";
 
 const UpdateProfile = () => {
   const { currentUser } = useSelector((state) => state.auth);
@@ -167,7 +36,7 @@ const UpdateProfile = () => {
     try {
       setLoading(true);
       const response = await axios.patch(
-        "http://127.0.0.1:8000/api/profile/update",
+        `${BASE_URL}/profile/update`,
         values,
         {
           headers: { Authorization: `Bearer ${authorisation.token}` },
