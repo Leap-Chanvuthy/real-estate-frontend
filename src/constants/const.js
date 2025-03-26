b1d933533287baca25b0ae6f40f5504c751fbaba
Elon Thy <115206732+Leap-Chanvuthy@users.noreply.github.com>
2025-03-26 09:22:34 +0700

    minor changes

--- src/constants/const.js
+++ src/constants/const.js
@@ -3,8 +3,8 @@
 // export const BASE_IMAGE_URL = 'http://localhost:8000/storage'
 // export const BASE_IMAGE_URL = 'https://dev-realestate.cammob.ovh/storage'
 
-const BASE_IMAGE_URL = 'https://real-estate.consultancyinspire.com/storage';
-const BASE_URL = 'https://real-estate.consultancyinspire.com/api';
+export const BASE_IMAGE_URL = 'https://real-estate.consultancyinspire.com/storage';
+export const BASE_URL = 'https://real-estate.consultancyinspire.com/api';
 
 
 export const DEFAULT_IMAGE = 'https://img.freepik.com/premium-vector/real-estate-business-concept-with-houses-real-estate-market-growth-flat-modern-design-illustration_566886-594.jpg'
